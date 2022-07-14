d8205e668f150eae6edd8fc72444f7580dd311ef
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-07-13 21:41:33 -0400

    Currently working on the destinations section

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -5,27 +5,17 @@ class CardView extends StatelessWidget {
   const CardView({Key? key, required this.cardImage}) : super(key: key);
 
   final String cardImage;
-  
+
   @override
   Widget build(BuildContext context) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 23.0, vertical: 23.0),
-        child: Container(
-          height: 400,
-          width: 300,
-          child: Stack(
-            children: [
-              ClipRRect(
-								borderRadius: BorderRadius.all(Radius.circular(12.0)),
-								child: Image.network(
-									cardImage,
-								),
-							),
-              
-							// Text for the picture
-            ],
+    return ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(12.0)),
+        child: Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: Image.network(
+            cardImage,
+            height: 340,
           ),
-        ),
-      );
-  } 
+        ));
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -37,94 +37,112 @@ class Home extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 23.0, top: 23.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
-                    ),
-                    SizedBox(width: 16),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Welcome'),
-                        SizedBox(height: 8),
-                        Text('Amanda Valdez'),
-                      ],
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: Icon(Icons.notifications),
-                ),
-              ],
-            )
-          ),
+              padding: const EdgeInsets.only(left: 23.0, top: 23.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                      ),
+                      SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Welcome'),
+                          SizedBox(height: 8),
+                          Text('Amanda Valdez'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    child: Icon(Icons.notifications),
+                  ),
+                ],
+              )),
           SizedBox(height: 25),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                hintText: 'Find your destination..',
-                enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.orange))
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              child: Container(
+                color: Colors.transparent,
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Find your destination..',
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12))),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12)))),
+                ),
               ),
             ),
           ),
           SizedBox(height: 25),
           Padding(
             padding: const EdgeInsets.only(left: 23.0),
-            child: Text('Featured', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            child: Text('Featured',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
           ),
-            Column(
-              mainAxisSize: MainAxisSize.max,
+
+          //? Listview for the destination cards
+
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
               children: [
-                Expanded(
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: [
-                      CardView(
-                        cardImage: 'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
-                      ),
-                      CardView(
-                                                cardImage:
-                            'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
-
-                      ),
-                      CardView(
-                                                cardImage:
-                            'https://cdn.pixabay.com/photo/2018/07/11/21/51/toast-3532016_1280.jpg',
-
-                      ),
-                    ],
-                  ),
+                CardView(
+                  cardImage:
+                      'https://images.unsplash.com/photo-1549716333-810d3c3fe7f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
                 ),
+                CardView(
+                  cardImage:
+                      'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1575&q=80',
+                ),
+                CardView(
+                  cardImage:
+                      'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1574&q=80',
+                ),
+                CardView(
+                  cardImage:
+                      'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1575&q=80',
+                )
               ],
             ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 23.0),
+            child: Text('Featured',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          ),
         ],
       ),
-      
-      
-
-      //? Listview for the destination cards
-      
-
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            backgroundColor: Colors.grey[900],
-            label: ''
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(23.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(32)),
+          child: BottomNavigationBar(
+              landscapeLayout: BottomNavigationBarLandscapeLayout.linear,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              elevation: 0,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home_filled),
+                    label: '',
+                    backgroundColor: Colors.grey),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.navigation), label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+              ]),
         ),
-        BottomNavigationBarItem(icon: Icon(Icons.navigation), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: ''),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
-      ]),
+      ),
     );
   }
 }
